029fea4598106f572fd982f141c50ed8d73f612b
dhanmatrixsecurities-Dmat <dhanmatrixsecurities@gmail.com>
2026-05-03 23:08:36 +0530

    Update _layout.tsx

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -2,7 +2,6 @@ import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { Colors } from '@/constants/Colors';
 import { StatusBar } from 'expo-status-bar';
-
 export default function TabLayout() {
   return (
     <>
@@ -30,6 +29,7 @@ export default function TabLayout() {
         <Tabs.Screen name="active-trades" options={{ title: 'Active Trades', tabBarIcon: ({ color, size }) => <Ionicons name="pulse" size={size} color={color} /> }} />
         <Tabs.Screen name="closed-trades" options={{ title: 'Closed Trades', tabBarIcon: ({ color, size }) => <Ionicons name="checkmark-done" size={size} color={color} /> }} />
         <Tabs.Screen name="ajeeb" options={{ title: '🤖 ai', headerTitle: 'Kooky ai', tabBarIcon: ({ color, size }) => <Ionicons name="sparkles" size={size} color={color} />, tabBarLabelStyle: { fontSize: 11, fontWeight: '600' } }} />
+        <Tabs.Screen name="portfolio-stocks" options={{ tabBarButton: () => null, title: 'Portfolio Stocks' }} />
         <Tabs.Screen name="profile" options={{ title: 'Profile', tabBarIcon: ({ color, size }) => <Ionicons name="person" size={size} color={color} /> }} />
       </Tabs>
     </>
